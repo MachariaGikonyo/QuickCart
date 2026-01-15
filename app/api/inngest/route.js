8b352e83ca0930201b23a94619e29e8d08dd9255
MachariaGikonyo <machariawilfred2@gmail.com>
2026-01-15 15:43:33 +0300

    Second Commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/injest";
+import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
