6ffc8c0b1d3d37ab3386cc6b3646950def60ef92
MachariaGikonyo <machariawilfred2@gmail.com>
2026-01-26 09:14:48 +0300

    16th commit

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -3,11 +3,15 @@ import Product from "@/models/Product";
 import User from "@/models/User";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+import connectDB from "@/config/db";
+
 
 
 
 export async function POST(request) {
     try {
+        await connectDB();
+
 
         const { userId } = getAuth(request)
         const { address, items } = await request.json();
@@ -17,11 +21,22 @@ export async function POST(request) {
         }
 
         // calculate amount using items
-        const amount = await items.reduce(async (acc, item) => {
-            const product = await Product.findById(item.product);
-            return await acc + product.offerPrice * item.quantity;
-        }, 0)
+        let amount = 0;
 
+        for (const item of items) {
+        const product = await Product.findById(item.product);
+
+        if (!product) {
+            return NextResponse.json({
+            success: false,
+            message: "Invalid product",
+            });
+        }
+
+        amount += product.offerPrice * item.quantity;
+        }
+
+        
         await inngest.send({
             name: 'order/created',
             data: {
